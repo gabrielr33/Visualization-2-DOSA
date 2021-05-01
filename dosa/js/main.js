00c7a2026248e46991a390db2bbbdbd79f4fafe3
Gabriel Ratschiller <gabriel.ratschiller1998@gmail.com>
2021-05-01 12:41:15 +0200

    add topojson, load data from europe json

--- dosa/js/main.js
+++ dosa/js/main.js
@@ -13,10 +13,45 @@ var flightListMonth = [];
 // Functions
 function init() {
     loadData(1901);
+    loadWorldMap();
 }
 
 function loadWorldMap() {
 
+    var countries;
+
+    //Load the world map from topojson
+    d3.json('./dataset/europe_map.json').then(function(worldData) {
+        if (worldData) {
+            countries = topojson.feature(worldData, worldData.objects.europe).features;
+            //console.log(countries);
+        }
+
+        var margin = {top: 50, left: 50, right:50, bottom:50},
+            height = 600 - margin.top - margin.bottom,
+            width = 1200 - margin.left - margin.right;
+
+        var projection = d3.geoMercator()
+            .translate([width/3, height*1.7])
+            .scale(500)
+
+        var geoPath = d3.geoPath()
+            .projection(projection)
+
+        let svg = d3.select("#map")
+            .append('svg')
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("width", width + margin.left + margin.right)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        svg.append('g').selectAll('path')
+            .data(countries)
+            .join('path')
+            .attr('d', geoPath)
+            .attr('fill', '#FFF')
+            .attr('stroke', '#000');
+    });
 }
 
 /**
@@ -26,29 +61,17 @@ function loadWorldMap() {
 function loadData(month) {
 
     //Load the specified monthly data
-    /*d3.csv("./dataset/dataset_flights_europe/flightlist_20" + month + ".csv").then(function (flightListMonth) {
-        flightListMonth.forEach(function (d) {
-            d.latitude_1 = +d.latitude_1;
-            d.longitude_1 = +d.longitude_1;
-            d.latitude_2 = +d.latitude_2;
-            d.longitude_2 = +d.longitude_2;
-        });
-        console.log("[DOSA] -> loaded " + flightListMonth.length + " flights for " + "20" + month);
-    });*/
+    d3.csv('./dataset/dataset_flights_europe/flightlist_20' + month + '.csv', function(loadedRow) {
+        if (loadedRow)
+            this.flightListMonth.push(loadedRow);
+    }).then(function() {
+        console.log(this.flightListMonth);
+    });
 
-    d3.queue()
+    /*d3.queue()
         .defer(d3.csv, "./dataset/dataset_flights_europe/flightlist_20" + month + ".csv")
-        .await(function (error, flightListMonth) {
-           if (error) console.log(error);
-
-           this.flightListMonth = flightListMonth;
-
-            this.flightListMonth.forEach(function (d) {
-                d.latitude_1 = +d.latitude_1;
-                d.longitude_1 = +d.longitude_1;
-                d.latitude_2 = +d.latitude_2;
-                d.longitude_2 = +d.longitude_2;
-            });
-            console.log("[DOSA] -> loaded " + this.flightListMonth.length + " flights for " + "20" + month);
-        });
+        .await((function (loadedRow) {
+            console.log("es");
+            if (loadedRow) this.flightListMonth.push(loadedRow);
+        }).bind(this));*/
 }
